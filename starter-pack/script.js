87a8971bb1d8253e7bf9a0a2e0b59c02caba1239
Magdalena Piotrowska <44752845+piotrowskamjr@users.noreply.github.com>
2019-12-09 22:31:26 +0100

    Add 3 homework

---
+++ starter-pack/script.js
@@ -0,0 +1,33 @@
+// Show/hide page navigation
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', (e) => {
+    const pageNavigation = document.querySelector('.page-navigation');
+    pageNavigation.classList.toggle('show-page-navigation');
+});
+
+// Mark ticket as bought
+const concerts = document.querySelector('.concerts');
+
+concerts.addEventListener('click', (e) => {
+    if (e.target.classList.contains('button--buy-ticket')) {
+        const buyTicketButton = e.target;
+        const haveFunText = document.createElement('p');
+        const concertsInfo = buyTicketButton.parentElement;
+
+        concertsInfo.append(haveFunText);
+        haveFunText.textContent = 'Have fun!';
+        concertsInfo.insertBefore(haveFunText, buyTicketButton);
+        buyTicketButton.remove();
+    };
+});
+
+// Log form data
+const contactForm = document.querySelector('.contact__form');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formObject = Object.fromEntries(formData);
+    console.log(Object.values(formObject));
+});
